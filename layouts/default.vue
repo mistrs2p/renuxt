58673c5e4fa4b00240c5ef28c1c1e62a4d8f8f6c
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 02:50:47 +0430

    Header Configuration Set

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
+    <NavTheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <NavTheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    />
     <Nuxt />
   </div>
 </template>
-
+<script>
+export default {
+  data () {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+</script>
 <style>
 html {
   font-family: IranSans;
