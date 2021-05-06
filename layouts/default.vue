38b5704eaecb6dd159b32838bdfdf43308461052
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-06 17:26:12 +0430

    Ubuntu Font Added

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,22 +6,7 @@
 
 <style>
 html {
-  font-family:
-    IranSans,
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-family: IranSans, Ubuntu;
   box-sizing: border-box;
   font-weight: 400;
 }
