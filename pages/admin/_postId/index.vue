7ea010acdb186492f169de2d1699f59ae385d0ce
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 06:20:40 +0430

    Style Added

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -21,6 +21,14 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
 </style>
