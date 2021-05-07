41fc8ae01cffefc79abfc7d1b829205d3a29f299
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 16:15:41 +0430

    Error Page Added

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="error-page">
+    <h1>مشکلی در اجرای درخواست به وجود آمده!!!</h1>
+    <p>بازگشت به <a href="/">محیط امن</a>!</p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
