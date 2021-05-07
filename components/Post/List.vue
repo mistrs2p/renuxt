bf69ba7a21b7938be01ceb9be2c2e002f948a7ba
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 06:11:16 +0430

    Direction For Admin Post Set

--- components/Post/List.vue
+++ components/Post/List.vue
@@ -1,18 +1,21 @@
 <template>
   <section class="posts-list">
-    <PostPreview 
+    <PostPreview
+      :is-admin="isAdmin" 
       id="1"
       title="سلاملکم"
       thumbnail="https://www.paymentsjournal.com/wp-content/uploads/2019/11/904-scaled.jpg"
       previewText="این اولین پستمه"
     />
-    <PostPreview 
+    <PostPreview
+      :is-admin="isAdmin" 
       id="2"
       title="چطوری"
       thumbnail="https://sm.mashable.com/mashable_in/seo/1/13445/13445_5s8f.jpg"
       previewText="این دومیشه"
     />
-    <PostPreview 
+    <PostPreview
+      :is-admin="isAdmin" 
       id="3"
       title="خوبی"
       thumbnail="https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg"
@@ -23,7 +26,12 @@
 
 <script>
 export default {
-
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
