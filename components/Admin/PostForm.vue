28ea28332ea1edd1c544dbac09d6af5730635637
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 05:58:07 +0430

    Post Form Component Added

---
+++ components/Admin/PostForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="onSave">
+    <UIAppControlInput v-model="editedPost.author">نام نویسنده</UIAppControlInput>
+    <UIAppControlInput v-model="editedPost.title">عنوان</UIAppControlInput>
+    <UIAppControlInput v-model="editedPost.thumbnailLink">لینک عکس</UIAppControlInput>
+    <UIAppControlInput
+      control-type="textarea"
+      v-model="editedPost.content">محتوا</UIAppControlInput>
+    <UIAppButton type="submit">ذخیره</UIAppButton>
+    <UIAppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel">صرف نظر</UIAppButton>
+  </form>
+</template>
+
+<script>
+export default {
+data () {
+    return {
+      editedPost: {
+      author: '',
+      title: '',
+      thumbnailLink: '',
+      content: ''
+      }
+    }
+  },
+  methods: {
+    onSave () {
+      console.log(this.editedPost)
+    },
+    onCancel () {
+      this.$router.push('/admin')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,44 +1,13 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <form @submit.prevent="onSave">
-        <UIAppControlInput v-model="editedPost.author">نام نویسنده</UIAppControlInput>
-        <UIAppControlInput v-model="editedPost.title">عنوان</UIAppControlInput>
-        <UIAppControlInput v-model="editedPost.thumbnailLink">لینک عکس</UIAppControlInput>
-        <UIAppControlInput
-          control-type="textarea"
-          v-model="editedPost.content">محتوا</UIAppControlInput>
-        <UIAppButton type="submit">ذخیره</UIAppButton>
-        <UIAppButton
-          type="button"
-          style="margin-left: 10px"
-          btn-style="cancel"
-          @click="onCancel">صرف نظر</UIAppButton>
-      </form>
+      <AdminPostForm />
     </section>
   </div>
 </template>
 
 <script>
 export default {
- data () {
-   return {
-     editedPost: {
-      author: '',
-      title: '',
-      thumbnailLink: '',
-      content: ''
-     }
-   }
- },
- methods: {
-   onSave () {
-     console.log(this.editedPost)
-   },
-   onCancel () {
-     this.$router.push('/admin')
-   }
- }
 }
 </script>
 
