01a245eaa136b74150457d8b9c4c599a7e47367d
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 16:03:53 +0430

    Auth Page Completed

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,13 +1,45 @@
 <template>
-  
+  <div class="admin-auth-page">
+    <div class="auth-container">
+      <form>
+        <UIAppControlInput type="email">آدرس ایمیل</UIAppControlInput>
+        <UIAppControlInput type="password">رمز عبور</UIAppControlInput>
+        <UIAppButton type="submit">{{ isLogin ? 'ورود' : 'ثبت نام' }}</UIAppButton>
+        <UIAppButton
+          type="button"
+          btn-style="inverted"
+          style="margin-left: 10px"
+          @click="isLogin = !isLogin">تغییر به {{ isLogin ? 'ثبت نام' : 'ورود' }}</UIAppButton>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'AdminAuthPage',
+  // layout: 'admin',
+  data() {
+    return {
+      isLogin: true
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.admin-auth-page {
+  padding: 20px;
+}
 
+.auth-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px #ccc;
+  width: 300px;
+  margin: auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
 </style>
+
