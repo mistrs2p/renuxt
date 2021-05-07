72d163f32b7a615b851bf17fe914f13471279671
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-07 05:37:28 +0430

    Admin Index Page Set

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="admin-page">
+    <section class="new-post">
+      <button>ایجاد پست جدید</button>
+    </section>
+    <section class="existing-posts">
+      <h1>پست های موجود</h1>
+      <PostList />
+    </section>
+  </div>
 </template>
 
 <script>
@@ -9,5 +17,17 @@ export default {
 </script>
 
 <style>
+  .admin-page {
+    padding: 20px;
+  }
 
+  .new-post {
+    text-align: center;
+    border-bottom: 2px solid #ccc;
+    padding-bottom: 10px;
+  }
+
+  .existing-posts h1 {
+    text-align: center;
+  }
 </style>
