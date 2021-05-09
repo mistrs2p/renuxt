78269338a60238963054820f317d91cfbd0f3d69
mistrs2p <39363439+mistrs2p@users.noreply.github.com>
2021-05-09 18:34:35 +0430

    Axios New Post Added And Som Correction Done

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'admin'
+  layout: 'admin',
+  methods: {
+    onSubmitted(postData) {
+      this.$axios.$post('https://nuxt-blog-fcdb2-default-rtdb.firebaseio.com/posts.json', postData)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
+  }
 }
 </script>
 
